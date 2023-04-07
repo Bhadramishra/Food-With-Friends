8c281179835d9c8a21892c8e8fe6631aeccc95e3
yaqubdiyaa <112641000+yaqubdiyaa@users.noreply.github.com>
2023-04-07 15:55:54 -0400

    all input shows up in review page (uses local storage)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 function submitPlan() {
-    window.localStorage.setItem('email',  document.getElementById(email));
-    window.localStorage.setItem('withWho', document.getElementById(withWho));
-    window.localStorage.setItem('meal',  document.getElementById(meal));
-    window.localStorage.setItem('inputMax',   document.getElementById(inputMax));
-    window.localStorage.setItem('time',  document.getElementById(time));
-    window.localStorage.setItem('date',  document.getElementById(date));
+    window.localStorage.clear();
+    window.localStorage.setItem('email',  document.getElementById("email").value);
+    window.localStorage.setItem('withWho', document.getElementById("withWho").value);
+    window.localStorage.setItem('meal',  document.getElementById("meal").value);
+    window.localStorage.setItem('inputMax',   document.getElementById("inputMax").value);
+    window.localStorage.setItem('time',  document.getElementById("time").value);
+    window.localStorage.setItem('date',  document.getElementById("date").value);
     window.location.replace("review.html");
 }
 
@@ -15,6 +16,11 @@ function getElems() {
     var inputMax = localStorage.getItem("inputMax");
     var time = localStorage.getItem("time");
     var date = localStorage.getItem("date");
-
     document.getElementById("email").innerHTML = email;
+    document.getElementById("withWho").innerHTML = withWho;
+    document.getElementById("meal").innerHTML = meal;
+    document.getElementById("inputMax").innerHTML = inputMax;
+    document.getElementById("time").innerHTML = time;
+    document.getElementById("date").innerHTML = date;
+
 }

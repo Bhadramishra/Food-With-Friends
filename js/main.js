48759ebd11dceefa0229d25d463db3b750c669a0
kavyald <kdoraswa@bowdoin.edu>
2023-04-08 17:04:23 -0400

    fixed date display

--- js/main.js
+++ js/main.js
@@ -132,6 +132,11 @@ function getElems() {
     var inputMax = localStorage.getItem("inputMax");
     var time = localStorage.getItem("time");
     var date = localStorage.getItem("date");
+
+    //display date in MM/DD/YYYY format
+    var dateSplit = date.split("-");
+    date = dateSplit[1]+"/"+dateSplit[2]+"/"+dateSplit[0]
+
     document.getElementById("email").innerHTML = email;
     document.getElementById("withWho").innerHTML = withWho;
     document.getElementById("meal").innerHTML = meal;

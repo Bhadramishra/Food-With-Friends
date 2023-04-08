0c4de98bb30005ac82b8f4c99b285f863e0a1b27
yaqubdiyaa <112641000+yaqubdiyaa@users.noreply.github.com>
2023-04-08 16:51:02 -0400

    trying to make classes in js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,90 @@
+class user {
+    constructor(name, withWho, numPpl, email, date, time) {
+      this.name = name; 
+      this.withWho = withWho; 
+      this.numPpl = numPpl;
+      this.email = email;
+      this.date = date;  
+      this.time = time; 
+    }
+
+    set name(name) {
+        this.name = name; 
+
+    }
+
+    set withWho(withWho) {
+        this.withWho = withWho; 
+
+    }
+
+    set numPpl(numPpl) {
+        this.numPpl = numPpl;
+
+    }
+
+    set email(email) {
+        this.email = email;
+
+    }
+
+    set date(date) {
+        this.date = date; 
+
+    }
+
+    set time(time) {
+        this.time = time; 
+    }
+
+    get name() {
+        return this.name; 
+    }
+
+    get withWho() {
+        return this.withWho;
+    }
+    
+
+    get numPpl() {
+        return this.numPpl;
+
+    }
+
+    get email() {
+        return this.email; 
+
+
+    }
+
+    get date() {
+        return this.date;
+
+    }
+
+    get time() {
+        return this.time; 
+    }
+
+  }
+  
+  Kavya = new user("Kavya", "Close", 5, "k@bowdoin.edu", "2023-04-08", "11:00am" );
+  Bhadra = new user("Bhadra", "Anyone", 2, "b@bowdoin.edu", "2023-05-08", "11:00am" );
+  Film = new user("Film", "Anyone", 1, "f@bowdoin.edu", "2023-04-08", "11:45am" );
+  Diyaa = new user("Diyaa", "Anyone", 10, "d@bowdoin.edu", "2023-04-08", "7:00am" );
+
+    
+
+  function otherUsers() {
+    //first somehow get from API
+    //store everyone who has submitted something (made post request) in a list
+    const users = [Kavya, Bhadra, Film, Diyaa]; 
+    window.localStorage.setItem("otherUsers", users); 
+    console.log(localStorage.getItem(users));
+  }
+
+
 function submitPlan() {
-    window.localStorage.clear();
     window.localStorage.setItem('email',  document.getElementById("email").value);
     window.localStorage.setItem('withWho', document.getElementById("withWho").value);
     window.localStorage.setItem('meal',  document.getElementById("meal").value);

e00a28a0df13b2e2099ceae908b34a81e4c330a3
yaqubdiyaa <112641000+yaqubdiyaa@users.noreply.github.com>
2023-04-09 13:11:20 -0400

    backend matches now working :)

--- js/main.js
+++ js/main.js
@@ -51,9 +51,9 @@ function submitPlan() {
         return false;
     }
     else {
-        window.location.replace("review.html");
-    }
-    
+    window.location.replace("review.html");
+}
+
 }
 
 <<<<<<< Updated upstream
@@ -99,20 +99,29 @@ function findMatch() {
 
 }
 
+/**
+ * Kavya = new user("Kavya", "Close", 5, "k@bowdoin.edu", "2023-04-08", "11:00am" );
+  Bhadra = new user("Bhadra", "Anyone", 2, "b@bowdoin.edu", "2023-05-08", "11:00am" );
+  Film = new user("Film", "Anyone", 1, "f@bowdoin.edu", "2023-04-08", "11:45am" );
+  Diyaa = new user("Diyaa", "Anyone", 10, "d@bowdoin.edu", "2023-04-08", "7:00am" );
+ */
+
 //this is called upon match found loading
 //this is how the matches are found (it iterates through the stored users, and compares to the values in local storage)
 function matches() {
     others = window.localStorage.getItem("otherUsers");
     console.log(others);
-    matches = [];
+    others = JSON.parse(others);
+    console.log(others);
+    let matches = [];
+
     var email = localStorage.getItem("email");
     var withWho = localStorage.getItem("withWho");
     var inputMax = localStorage.getItem("inputMax");
     var time = localStorage.getItem("time");
     var date = localStorage.getItem("date");
     var meal = localStorage.getItem("meal");
-    console.log(date);
-    console.log(meal);
+
     for (let i = 0; i < others.length; i++) {
         if (matches.length == inputMax) {
             break; 
@@ -122,22 +131,33 @@ function matches() {
             continue;
         }
 
+
         if (others[i].date != date) {
             continue;
         }
+
 
         if (others[i].time != time) {
             continue;
         }
 
         //when date and time match 
-        if (others[i].inputMax < matches.length) {
+        if (others[i].numPpl > matches.length) {
             matches.push(others[i]);
+
         } 
     }
 
-    document.getElementById("matches").innerHTML = matches;
     console.log(matches);
+
+    var names = "";
+    for (i = 0; i < matches.length; i++) {
+        var name = matches[i].name;
+        names += "<li>"+name+"</li>";
+
+    }
+
+    document.getElementById("matches").innerHTML = names;
     document.getElementById("Meal").innerHTML = "Meal: " + meal;
     document.getElementById("Date").innerHTML = "Date: " + date;
     document.getElementById("Time").innerHTML = "Time: " + time;
